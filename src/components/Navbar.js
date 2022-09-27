93a2edfd6c42c80b87a51e54290e9e1884db7a66
Rushabh Gandhi <rushabh.dg@somaiya.edu>
2022-09-27 12:50:04 +0530

    HRN

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,20 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dapp University's Blockchain Marketplace
+      <div className="leftside">
+      <a
+        className="navbar-brand col-sm-3 col-md-2 mr-4"
+        href="http://www.dappuniversity.com/bootcamp"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{fontSize: "30px"}}
+      >
+        HRN
+      </a>
+        <a style={{marginLeft: '4vw', color: 'white'}}>
+          About
         </a>
+      </div>  
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-white"><span id="account">{this.props.account}</span></small>
@@ -23,4 +29,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default Navbar
